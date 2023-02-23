6fa6daa94191ecf023e7d7f60a73f91157487416
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-23 10:50:14 -0300

    Update Temporizador

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+				<!-- O : antes do disabled vincula ele ao estado. -->
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+		<!-- O : antes do disabled vincula ele ao estado. -->
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando"> 
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Temporizador",
+  components: {
+    Cronometro,
+  },
+
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+			cronometroRodando: false,
+    };
+  },
+
+  methods: {
+    iniciar() {
+			this.cronometroRodando = true;
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos++;
+      }, 1000);
+    },
+    finalizar() {
+			this.cronometroRodando = false;
+      clearInterval(this.cronometro);
+    },
+  },
+});
+</script>
