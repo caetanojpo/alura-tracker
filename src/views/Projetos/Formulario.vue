55209161527e347800ea70977cd53b7522032bae
João Pedro Caetano <caetanojpo@gmail.com>
2023-03-07 10:15:22 -0300

    rotas aninhadas e refatoração do Projetos

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,6 +1,5 @@
 <template>
-  <section class="projetos">
-    <h1 class="title">Projetos</h1>
+  <section>
     <form @submit.prevent="salvar">
       <div class="field">
         <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
@@ -66,9 +65,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style scoped>
-.projetos {
-  padding: 1.5rem;
-}
-</style>
